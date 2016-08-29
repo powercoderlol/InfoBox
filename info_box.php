5e3c4d47dec51dccea857cd2e2c7bf5861615753
Ivan <ipolyiakov@gmail.com>
2016-08-29 13:25:47 +0300

    fix info_box_auto_br and add do_shortcode

--- info_box.php
+++ info_box.php
@@ -496,302 +496,165 @@ function create_info_box_template($id)
 		$curr_content_style = "background-image: url(".$result['contentImgUrl']."); ";
 	}
 
-	$template = '<div class="coderlol-info-box"><div class="coderlol-info-box-title" style="'.$curr_title_style.'"><div class="coderlol-new-title-box">' . $result['title'] . '</div></div><div class="coderlol-info-box-content" style="'.$curr_content_style.'">' .wpautop($result['content']).'</div></div>';
+	$template = '<div class="coderlol-info-box"><div class="coderlol-info-box-title" style="'.$curr_title_style.'"><div class="coderlol-new-title-box">' . $result['title'] . '</div></div><div class="coderlol-info-box-content" style="'.$curr_content_style.'">' .info_box_auto_br(do_shortcode($result['content'])).'</div></div>';
 
 	return $template;
 
 }
 
-/*
-function create_note_desk_template()
-{
-	global $wpdb;
 
 
 
 
+function info_box_auto_br( $pee, $br = true ) {
+	$pre_tags = array();
 
-	$coderlol_box_title = get_option('coderlol_desk_title');
-	$coderlol_desk_title_image = get_option('coderlol_desk_title_image');
-	$coderlol_desk_image = get_option('coderlol_desk_image');
-	$coderlol_desk_title_color = "#".get_option('coderlol_desk_title_color');
-	$coderlol_desk_color = "#".get_option('coderlol_desk_color');
-	$coderlol_note_title_height = get_option('coderlol_note_title_height');
-	$coderlol_desk_title_grad_position = get_option('coderlol_desk_title_grad_position');
-	$coderlol_desk_grad_position = get_option('coderlol_desk_grad_position');
-	$coderlol_desk_title_color_grad = "#".get_option('coderlol_desk_title_color_grad');
-	$coderlol_desk_color_grad = "#".get_option('coderlol_desk_color_grad');
-	$coderlol_desk_grad = get_option('grad_desk');
-	$coderlol_desk_title_grad = get_option('grad_desk_title');
+	if ( trim($pee) === '' )
+		return '';
 
-	$curr_title_style = "";
-	$curr_desk_style = "";
+	// Just to make things a little easier, pad the end.
+	$pee = $pee . "\n";
 
-
-/************************************STYLE FOR TITLE FROM SETTINGS**************************/
-/*
-	if ($coderlol_desk_title_image == null)
-	{
-		$curr_title_style = "background-color: ".$coderlol_desk_title_color."; ";
-	}
-	else 
-	{
-		$curr_title_style = "background-image: url(".$coderlol_desk_title_image."); ";
-	}
-
-	$curr_title_style = $curr_title_style."height: ".$coderlol_note_title_height."px; ";
-
-
-	if($coderlol_desk_title_grad != '0')
-	{
-		$curr_title_style = "background: linear-gradient(".$coderlol_desk_title_grad_position.", ".$coderlol_desk_title_color.", ".$coderlol_desk_title_color_grad."); ";
-	}
-
-/************************************STYLE FOR DESK CONTENT FROM SETTINGS********************/
-/*
-
-	if ($coderlol_desk_image == null)
-	{
-		$curr_desk_style = "background-color: ".$coderlol_desk_color.";";
-	}
-	else 
-	{
-		$curr_desk_style = "background-image: url(".$coderlol_desk_image.");";
-	}
-
-
-	if($coderlol_desk_grad != '0')
-	{
-		$curr_desk_style = "background: linear-gradient(".$coderlol_desk_grad_position.", ".$coderlol_desk_color.", ".$coderlol_desk_color_grad."); ";
-	}
-
-
-/************************************GENERATOR*********************************************/
-/*
-
-	$template = $temp.'<div class="coderlol-note-box"><div class="coderlol-note-box-title" style="'.$curr_title_style.'"><div class="coderlol-new-title-box">' . $coderlol_box_title . '</div></div><div class="coderlol-note-box-content" style="'.$curr_desk_style.'">';
-
-
-	$table_notes = $wpdb->prefix . "coderlol_notes";
-
-	/**
-	markers pattern common category
-	*/
 	/*
+	 * Pre tags shouldn't be touched by autop.
+	 * Replace pre tags with placeholders and bring them back after autop.
+	 */
+	if ( strpos($pee, '<pre') !== false ) {
+		$pee_parts = explode( '</pre>', $pee );
+		$last_pee = array_pop($pee_parts);
+		$pee = '';
+		$i = 0;
 
-	$notes = $wpdb->get_results("SELECT id, title, content, category, color, fontSize, rotation, grad, gradColor, gradPosition, date, updateDate,  CASE WHEN (date > updateDate) THEN date WHEN ISNULL(updateDate) THEN date ELSE updateDate END AS mainDate FROM ".$table_notes." ORDER BY 
-		mainDate DESC");
+		foreach ( $pee_parts as $pee_part ) {
+			$start = strpos($pee_part, '<pre');
 
+			// Malformed html?
+			if ( $start === false ) {
+				$pee .= $pee_part;
+				continue;
+			}
 
-	$next_counter = 2;
+			$name = "<pre wp-pre-tag-$i></pre>";
+			$pre_tags[$name] = substr( $pee_part, $start ) . '</pre>';
 
-
-
-	//$template = $template . "<div class='coderlol-common-category'>";
-
-	foreach ($notes as $item)
-	{
-
-		if($next_counter == 2)
-		{
-			$template = $template . "<div class='coderlol-common-category'>";
-			$template = $template . generate_note($item);
-			$next_counter = 1;
+			$pee .= substr( $pee_part, 0, $start ) . $name;
+			$i++;
 		}
-		else
-		{
-			$template = $template . generate_note($item);
-			$template = $template . '</div>';
-			$next_counter = 2;
-		}
+
+		$pee .= $last_pee;
 	}
-	
+	// Change multiple <br>s into two line breaks, which will turn into paragraphs.
+	$pee = preg_replace('|<br\s*/?>\s*<br\s*/?>|', "\n\n", $pee);
 
-	$template = $template. '</div></div>';
+	$allblocks = '(?:table|thead|tfoot|caption|col|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|form|map|area|blockquote|address|math|style|p|h[1-6]|hr|fieldset|legend|section|article|aside|hgroup|header|footer|nav|figure|figcaption|details|menu|summary)';
 
-	$template = do_shortcode($template);
+	// Add a single line break above block-level opening tags.
+	$pee = preg_replace('!(<' . $allblocks . '[^>]*>)!', "\n$1", $pee);
 
-	return $template;
+	// Add a double line break below block-level closing tags.
+	$pee = preg_replace('!(</' . $allblocks . '>)!', "$1\n\n", $pee);
 
+	// Standardize newline characters to "\n".
+	$pee = str_replace(array("\r\n", "\r"), "\n", $pee);
+
+	// Find newlines in all elements and add placeholders.
+	$pee = wp_replace_in_html_tags( $pee, array( "\n" => " <!-- wpnl --> " ) );
+
+	// Collapse line breaks before and after <option> elements so they don't get autop'd.
+	if ( strpos( $pee, '<option' ) !== false ) {
+		$pee = preg_replace( '|\s*<option|', '<option', $pee );
+		$pee = preg_replace( '|</option>\s*|', '</option>', $pee );
+	}
+
+	/*
+	 * Collapse line breaks inside <object> elements, before <param> and <embed> elements
+	 * so they don't get autop'd.
+	 */
+	if ( strpos( $pee, '</object>' ) !== false ) {
+		$pee = preg_replace( '|(<object[^>]*>)\s*|', '$1', $pee );
+		$pee = preg_replace( '|\s*</object>|', '</object>', $pee );
+		$pee = preg_replace( '%\s*(</?(?:param|embed)[^>]*>)\s*%', '$1', $pee );
+	}
+
+	/*
+	 * Collapse line breaks inside <audio> and <video> elements,
+	 * before and after <source> and <track> elements.
+	 */
+	if ( strpos( $pee, '<source' ) !== false || strpos( $pee, '<track' ) !== false ) {
+		$pee = preg_replace( '%([<\[](?:audio|video)[^>\]]*[>\]])\s*%', '$1', $pee );
+		$pee = preg_replace( '%\s*([<\[]/(?:audio|video)[>\]])%', '$1', $pee );
+		$pee = preg_replace( '%\s*(<(?:source|track)[^>]*>)\s*%', '$1', $pee );
+	}
+
+	// Remove more than two contiguous line breaks.
+	$pee = preg_replace("/\n\n+/", "\n\n", $pee);
+
+	// Split up the contents into an array of strings, separated by double line breaks.
+	$pees = preg_split('/\n\s*\n/', $pee, -1, PREG_SPLIT_NO_EMPTY);
+
+	// Reset $pee prior to rebuilding.
+	$pee = '';
+
+	// Rebuild the content as a string, wrapping every bit with a <p>.
+	foreach ( $pees as $tinkle ) {
+		$pee .= '' . trim($tinkle, "\n") . "\n";
+	}
+
+	// Under certain strange conditions it could create a P of entirely whitespace.
+	$pee = preg_replace('|<p>\s*</p>|', '', $pee);
+
+	// Add a closing <p> inside <div>, <address>, or <form> tag if missing.
+	$pee = preg_replace('!<p>([^<]+)</(div|address|form)>!', "<p>$1</p></$2>", $pee);
+
+	// If an opening or closing block element tag is wrapped in a <p>, unwrap it.
+	$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+
+	// In some cases <li> may get wrapped in <p>, fix them.
+	$pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee);
+
+	// If a <blockquote> is wrapped with a <p>, move it inside the <blockquote>.
+	$pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
+	$pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
+
+	// If an opening or closing block element tag is preceded by an opening <p> tag, remove it.
+	$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee);
+
+	// If an opening or closing block element tag is followed by a closing <p> tag, remove it.
+	$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+
+	// Optionally insert line breaks.
+	if ( $br ) {
+		// Replace newlines that shouldn't be touched with a placeholder.
+		$pee = preg_replace_callback('/<(script|style).*?<\/\\1>/s', '_autop_newline_preservation_helper', $pee);
+
+		// Normalize <br>
+		$pee = str_replace( array( '<br>', '<br/>' ), '<br />', $pee );
+
+		// Replace any new line characters that aren't preceded by a <br /> with a <br />.
+		$pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee);
+
+		// Replace newline placeholders with newlines.
+		$pee = str_replace('<WPPreserveNewline />', "\n", $pee);
+	}
+
+	// If a <br /> tag is after an opening or closing block tag, remove it.
+	$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee);
+
+	// If a <br /> tag is before a subset of opening or closing block tags, remove it.
+	$pee = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee);
+	$pee = preg_replace( "|\n</p>$|", '</p>', $pee );
+
+	// Replace placeholder <pre> tags with their original content.
+	if ( !empty($pre_tags) )
+		$pee = str_replace(array_keys($pre_tags), array_values($pre_tags), $pee);
+
+	// Restore newlines in all elements.
+	if ( false !== strpos( $pee, '<!-- wpnl -->' ) ) {
+		$pee = str_replace( array( ' <!-- wpnl --> ', '<!-- wpnl -->' ), "\n", $pee );
+	}
+
+	return $pee;
 }
-
-function generate_note($item)
-{
-
-	$atts = array (
-		'current_title' => $item->title,
-		'current_content' => $item->content,
-		'current_date' => date("d.m.y", strtotime($item->date))
-	);
-
-	if ($item->rotation == '1')
-	{
-		$rotation = 'transform: rotate(1deg);';
-	}
-	elseif ($item->rotation == '0')
-	{
-		$rotation = 'transform: rotate(-1deg);';
-	}
-	else 
-	{
-		$rotation = 'transform: rotate(0deg);';
-	}
-
-
-	$curr_date = new DateTime();
-	$note_date = new DateTime(date("Y-m-d", strtotime($item->date)));
-	//echo $curr_date->format('Y-m-d\TH:i:s.u');
-	$interval = $note_date->diff($curr_date);
-	
-
-	/**
-		NEW MARKER
-	*/
-		/*
-	if ($interval->days < 7) 
-	{
-		$new_marker = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: rgba(14, 206, 91, 0.76);">НОВОЕ</label>';
-	}
-	else
-	{
-		$new_marker ='';
-	}
-	/**
-		CATEGORY MARKER
-	*/
-		/*
-	if($item->category == '1')
-	{
-		$important_marker = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: rgba(255, 0, 0, 0.95);">ВАЖНОЕ</label>';
-	}
-	else
-	{
-		$important_marker = '';
-	}
-	/**
-		UPDATE DATE MARKER
-	*/
-		/*
-	if($item->updateDate != null)
-	{
-		$update_date = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: rgb(6, 212, 202);"><i class="fa fa-pencil-square-o" aria-hidden="true" style="margin-right: 5px;"></i>'.date("d.m.Y", strtotime($item->updateDate)).'</label>';
-	}
-	else
-	{
-		$update_date = '';
-	}
-
-
-	if ($item->grad == '1')
-	{
-		$curr_color_style = "background: linear-gradient(".$item->gradPosition.", ".$item->color.", ".$item->gradColor.");";
-	}
-	else
-	{
-		$curr_color_style = "background-color: ".$item->color.";";
-	}
-
-	$date_marker = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: #D0C2C2; margin-left: 0px !important;">'.date("d.m.Y", strtotime($item->date)).'</label>';
-
-	return '<div class="coderlol-note-outer"><div class="coderlol-note" style="'.$rotation.'"><div class="coderlol-note-inner" style="'.$curr_color_style.'"><label class="coderlol-note-title" style="font-size: '.$item->fontSize.'px;">'.$item->title.'</label><br><hr noshade size="1"></hr><div class="coderlol-note-inner-content" style="font-size: '.$item->fontSize.'px;">'.$item->content.'</div><br><br><div style="overflow: hidden; display: block;">'.$date_marker . $update_date . $new_marker . $important_marker .'</div></div></div></div>';
-
-}
-
-/**
-
-GENERATE NOTE PREVIEW BEGIN
-
-*/
-
-/*
-
-function generate_note_preview($note_atts, $date)
-{
-
-	$curr_date_object = new DateTime();
-	$note_date_object = $date;
-	$note_date_string = $note_date_object->format("d.m.Y");
-
-
-	if ($note_atts['note_rotation'] == '1')
-	{
-		$rotation = 'transform: rotate(1deg); ';
-	}
-	elseif ($note_atts['note_rotation'] == '0')
-	{
-		$rotation = 'transform: rotate(-1deg); ';
-	}
-	else 
-	{
-		$rotation = 'transform: rotate(0deg); ';
-	}
-
-
-
-
-	//$note_date = new DateTime();
-	//echo $curr_date->format('Y-m-d\TH:i:s.u');
-	$interval = $note_date_object->diff($curr_date_object);
-	
-	if ($interval->days < 7) 
-	{
-		$new_marker = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: #00FF66;">НОВОЕ</label>';
-	}
-	else
-	{
-		$new_marker ='';
-	}
-
-	/**
-		CATEGORY MARKER
-	*/
-		/*
-	if($note_atts['category'] == '1')
-	{
-		$important_marker = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: rgba(255, 0, 0, 0.95);">ВАЖНОЕ</label>';
-	}
-	else
-	{
-		$important_marker = '';
-	}
-	/**
-		UPDATE DATE MARKER
-	*/
-		/*
-	if($note_atts['update_date'] != null)
-	{
-		$update_date = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: rgb(6, 212, 202);"><i class="fa fa-pencil-square-o" aria-hidden="true" style="margin-right: 5px;"></i>'.$note_atts['update_date'].'</label>';
-	}
-	else
-	{
-		$update_date = '';
-	}
-
-	if ($note_atts['grad'] == '1')
-	{
-		$curr_color_style = "background: linear-gradient(".$note_atts['grad_position'].", ".$note_atts['color'].", ".$note_atts['grad_color'].");";
-	}
-	else
-	{
-		$curr_color_style = "background-color: ".$note_atts['color'].";";
-	}
-
-
-	$date_marker = '<label id="coderlol-date-label" class="coderlol-label" style="background-color: #D0C2C2; margin-left: 0px !important;">'.date("d.m.Y", strtotime($note_date_string)).'</label>';
-
-	return '<div class="coderlol-note-outer"><div class="coderlol-note" style="'.$rotation.'"><div class="coderlol-note-inner" style="'.$curr_color_style.'"><label class="coderlol-note-title" style="font-size: '.$note_atts['font_size'].'px;">'.$note_atts['title'].'</label><br><hr noshade size="1"></hr><div class="coderlol-note-inner-content" style="font-size: '.$note_atts['font_size'].'px;">'.$note_atts['content'].'</div><br><br><div style="overflow: hidden; display: block;">'.$date_marker . $update_date . $new_marker . $important_marker .'</div></div></div></div>';
-
-}
-
-
-
-
-
-/**
-
-GENERATE NOTE PREVIEW END
-
-*/
 
 
 /***************************SHORTCODE FOR DESK***************************/
